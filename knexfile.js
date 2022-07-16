581f40a01f322f65192878d6d44d6fc19df928df
budianonym <budianonym@gmail.com>
2022-07-16 10:27:18 +0700

    adjustment1

--- knexfile.js
+++ knexfile.js
@@ -3,12 +3,31 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
- import env from './env.js';
- import config from './../../../config/database.js';
+import dotenv from 'dotenv';
+dotenv.config({ silent: true, path: './../../../.env' })
+// import config from './../../../config/database.js';
+
+let setConfig = {
+  // ...config, 
+  migrations: {
+    directory: './../../../database/migrations'
+  },
+  seeds: {
+    directory: './../../../database/seeds'
+  },
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  },
+}
 export default {
 
-  development: config,
-  staging: config,
-  production: config
+  development: setConfig,
+  staging: setConfig,
+  production: setConfig
 
 };
