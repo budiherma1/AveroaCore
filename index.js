1e7376f35f8e8292a292b8eb561ad6c4166c329c
budianonym <budianonym@gmail.com>
2023-01-21 16:07:56 +0700

    optimize and fixing async process

--- index.js
+++ index.js
@@ -58,8 +58,8 @@ export const start = async () => {
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(cookieParser())
   
-  app.use('/api', apiRoute)
-  app.use('/', webRoute)
+  app.use('/api', await apiRoute)
+  app.use('/', await webRoute)
   // app.use('/', csrfProtection, webRoute)
 
   app.use('/', express.static(path.join(__dirname, '/public')));
