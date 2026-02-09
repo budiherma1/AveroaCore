082ebb8d2758ea8e2f7eeb8cb988fd3a013cdda8
budiherma1 <budianonym@gmail.com>
2026-02-10 05:27:30 +0700

    adjust cors to be support wildcard

--- cors.js
+++ cors.js
@@ -1,15 +1,39 @@
 import cors from 'cors';
 
-const whitelist = process.env.CORS_WHITELIST.split(",")
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
+const whitelist = process.env.CORS_WHITELIST
+  ?.split(',')
+  .map(origin => origin.trim());
+
+function isAllowedOrigin(origin) {
+  if (!origin) return true; // allow non-browser requests
+
+  return whitelist.some(allowed => {
+    // exact match
+    if (!allowed.includes('*')) {
+      return origin === allowed;
     }
-  }
+
+    // wildcard match (*.domain.com)
+    const regex = new RegExp(
+      '^https?:\\/\\/([a-z0-9-]+\\.)?' +
+      allowed.replace('*.', '').replace('.', '\\.') +
+      '$',
+      'i'
+    );
+
+    return regex.test(origin);
+  });
 }
 
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (isAllowedOrigin(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
 export default cors(corsOptions);
-// export default cors();
